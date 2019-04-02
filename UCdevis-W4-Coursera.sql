8a5581913e1e03cacd384ad6d5b6f124df52ffe6
Mohamed Gamal Hamed <47461269+gemyhamed@users.noreply.github.com>
2019-04-03 00:50:50 +0200

    Update UCdevis-W4-Coursera.sql

--- UCdevis-W4-Coursera.sql
+++ UCdevis-W4-Coursera.sql
@@ -1,3 +1,9 @@
+These Are my answers to the 'SQL for Data Science ,University of California, Davis' Course on Coursera 
+Course Link : https://www.coursera.org/learn/sql-for-data-science/home/welcome
+
+#Week4
+
+
 All of the questions in this quiz refer to the open source Chinook Database. Please familiarize yourself with the ER diagram in order to familiarize yourself with the table and column names in order to write accurate queries and get the appropriate answers.
 
 (https://ucde-rey.s3.amazonaws.com/DSV1015/ChinookDatabaseSchema.png)
